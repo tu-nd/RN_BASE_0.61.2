a658be131f21ea1a6411213d7412a4d2aee0c2a8
nguyen_tu <tundk57@gmail.com>
2020-11-26 17:15:56 +0700

    fomat code, refactor navigation, add NotificationScreen

--- SOURCE/APP/app/constants/Constant.js
+++ SOURCE/APP/app/constants/Constant.js
@@ -2,12 +2,12 @@ const SCREEN_ROUTER = {
   MAIN: "Main",
   AUTH: "Auth",
   HOME: "Home",
+  NOTIFICATION: "Notification",
   LOGIN: "Login",
   AUTH_LOADING: "AuthLoading",
   FORGOT_PASS: "ForgotPassword",
   REGISTER: "Register",
-  USER : 'User'
+  USER: "User"
 };
 
 export { SCREEN_ROUTER };
-
