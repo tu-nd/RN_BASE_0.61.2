088e7967602b11449e32ab899b3df513dea2067d
nguyen_tu <tundk57@gmail.com>
2020-11-28 14:49:02 +0700

    commit

--- SOURCE/0.61.2/app/screens/customer/HomeScreen.js
+++ SOURCE/0.61.2/app/screens/customer/HomeScreen.js
@@ -30,7 +30,7 @@ export default class HomeScreen extends Component {
                 showModal: true
               });
             }}
-            children={<Text>RNCA Home </Text>}
+            children={<Text>RNCA Home1 </Text>}
           />
         </SafeAreaView>
       </View>
